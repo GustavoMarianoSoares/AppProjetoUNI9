84ca6ec9e65e4aaa064af7a2d2e0205952a62ebd
Mariano <gustavomarianosoares@gmail.com>
2023-04-04 11:40:08 -0300

    Selecionar aberto e fechados

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -13,6 +13,7 @@ import firestore from "@react-native-firebase/firestore"
 
 export function Home({ navigation }) {
   const [orders, setOrders] = useState([])
+  const [status, setStatus] = useState('open')
 
   function handleDelete(id) {
     Alert.alert('', 'Tem certeza que deseja deletar este chamado?', [
@@ -28,28 +29,6 @@ export function Home({ navigation }) {
             .delete()
             .then(() => {
               Alert.alert('Deletado', 'Chamado deletado com sucesso')
-            })
-      },
-    ]);
-
-  }
-
-  function handleClosed(id) {
-    Alert.alert('', 'Tem certeza que deseja fechar este chamado?', [
-      {
-        text: 'Cancelar',
-      },
-      {
-        text: 'Ok',
-        onPress: () =>
-          firestore()
-            .collection('orders')
-            .doc(id)
-            .update({
-              status: 'closed'
-            })
-            .then(() => {
-              Alert.alert('Concluído', 'Chamado concluído com sucesso')
             })
       },
     ]);
@@ -81,6 +60,35 @@ export function Home({ navigation }) {
     return () => subscriber();
   }, []);
 
+  const renderStatus =
+    orders.map((item) =>
+      item.status == status ?
+        <View key={item.key} style={styles.container}>
+          <View>
+            <Text style={styles.component}>{item.component}</Text>
+            <Text style={styles.orderInfos}>{item.description}</Text>
+            <Text style={styles.orderInfos}>{item.owner}</Text>
+            <Text style={styles.orderInfos}>{item.telephoneOwner}</Text>
+            <Text style={styles.orderInfos}>{item.patrimony}</Text>
+          </View>
+
+          <View>
+            <TouchableOpacity
+              style={styles.icons}
+              onPress={() => openEditOrder(item)}>
+              <Feather name="edit" size={35} color="white" />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.icons}
+              onPress={() => handleDelete(item.key)}>
+              <Feather name="trash-2" size={35} color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
+        : <Text></Text>
+    )
+
   return (
     <ScrollView>
       <Header />
@@ -90,39 +98,13 @@ export function Home({ navigation }) {
         onPress={openCreateOrder}
       />
 
+      <TouchableOpacity onPress={() => setStatus('open')}><Text>Aberto</Text></TouchableOpacity>
+      <TouchableOpacity onPress={() => setStatus('closed')}><Text>Fechado</Text></TouchableOpacity>
+
       {
-        orders.map((item) =>
-            <View key={item.key} style={styles.container}>
-              <View>
-                <Text style={styles.component}>{item.component}</Text>
-                <Text style={styles.orderInfos}>{item.description}</Text>
-                <Text style={styles.orderInfos}>{item.owner}</Text>
-                <Text style={styles.orderInfos}>{item.telephoneOwner}</Text>
-                <Text style={styles.orderInfos}>{item.patrimony}</Text>
-              </View>
-
-              <View>
-                <TouchableOpacity
-                  style={styles.icons}
-                  onPress={() => openEditOrder(item)}>
-                  <Feather name="edit" size={35} color="white" />
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                  style={styles.icons}
-                  onPress={() => handleDelete(item.key)}>
-                  <Feather name="trash-2" size={35} color="white" />
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                  style={styles.icons}
-                  onPress={() => handleClosed(item.key)}>
-                  <Octicons name="issue-closed" size={35} color="white" />
-                </TouchableOpacity>
-              </View>
-            </View>
-        )
+        renderStatus
       }
+
     </ScrollView>
   )
 }
