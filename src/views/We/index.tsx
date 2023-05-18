1502cd863cdaad18c2704da4e90f3f8a8f0a7df0
unknown <gustavomarianosoares@gmail.com>
2023-05-18 10:31:23 -0300

    Descricao de quem somos

--- src/views/We/index.tsx
+++ src/views/We/index.tsx
@@ -1,16 +1,19 @@
 import { Text, View } from "react-native";
 import React from "react";
 
+import styles from "./styles"
+
 export function We() {
   return (
     <View>
-      <View>
-        <Text>Gustavo Mariano Soares</Text>
-        <Text>Breno Marcos da Silva Vela</Text>
+      <View style={styles.names}>
+        <Text style={styles.namesText}>Gustavo Mariano Soares</Text>
+        <Text style={styles.namesText}>Breno Marcos da Silva Vela</Text>
       </View>
 
-      <View>
-        <Text>Gustavo Mariano Soares</Text>
+      <View style={styles.details}>
+        <Text>Empresa criada em 2023 com o intuito de ajudar donos/CEO de lojas de Help desk onde ajuda com a organização de componentes para serem arrumados, limpos, melhorados, montados e etc...
+        </Text>
       </View>
     </View>
   );
---
+++ src/views/We/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  names: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  namesText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  details: {
+    alignItems: "center",
+    marginTop: 50,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+});
